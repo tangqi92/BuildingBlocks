d8105f84e112a7f37a023e528e018962044c8c28
tangqi92 <imtangqi@gmail.com>
2015-08-18 17:42:32 +0800

    Optimiazaiton: Using design.FloatingActionButton instead of third party #35

---
+++ app/src/main/java/me/itangqi/buildingblocks/utils/Utils.java
@@ -0,0 +1,22 @@
+package me.itangqi.buildingblocks.utils;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+import me.itangqi.buildingblocks.R;
+
+public class Utils {
+
+    public static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+
+    public static int getTabsHeight(Context context) {
+        return (int) context.getResources().getDimension(R.dimen.tabsHeight);
+    }
+}
---
+++ app/src/main/java/me/itangqi/buildingblocks/widget/ScrollingFABBehavior.java
@@ -0,0 +1,38 @@
+package me.itangqi.buildingblocks.widget;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+import me.itangqi.buildingblocks.utils.Utils;
+
+
+public class ScrollingFABBehavior extends FloatingActionButton.Behavior {
+    private int toolbarHeight;
+
+    public ScrollingFABBehavior(Context context, AttributeSet attrs) {
+        super();
+        this.toolbarHeight = Utils.getToolbarHeight(context);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        return super.layoutDependsOn(parent, fab, dependency) || (dependency instanceof AppBarLayout);
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        boolean returnValue = super.onDependentViewChanged(parent, fab, dependency);
+        if (dependency instanceof AppBarLayout) {
+                CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
+                int fabBottomMargin = lp.bottomMargin;
+                int distanceToScroll = fab.getHeight() + fabBottomMargin;
+                float ratio = (float)dependency.getY()/(float)toolbarHeight;
+                fab.setTranslationY(-distanceToScroll * ratio);
+        }
+        return returnValue;
+    }
+}
