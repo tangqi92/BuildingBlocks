a284f1565ef49c3344951899b54333020bf8eecf
Troy Liu <troyliu0105@outlook.com>
2015-09-18 22:46:24 +0800

    修复因为System.exit(0)而引起没有关闭动画的BUG

--- app/src/main/java/me/itangqi/buildingblocks/ui/activity/MainActivity.java
+++ app/src/main/java/me/itangqi/buildingblocks/ui/activity/MainActivity.java
@@ -218,7 +218,6 @@ public class MainActivity extends BaseActivity {
                 exitTime = System.currentTimeMillis();
             } else {
                 finish();
-                System.exit(0);
             }
             return true;
         }
