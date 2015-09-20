f1b7ad3bdeb5ee39f67ab6af9452faf0332674bc
Troy Liu <troyliu0105@outlook.com>
2015-09-20 20:51:25 +0800

    add litepal.xml

--- app/src/main/java/me/itangqi/buildingblocks/application/App.java
+++ app/src/main/java/me/itangqi/buildingblocks/application/App.java
@@ -3,10 +3,12 @@ package me.itangqi.buildingblocks.application;
 import android.app.Application;
 import android.content.Context;
 
+import org.litepal.LitePalApplication;
+
 /**
  * Created by tangqi on 7/20/15.
  */
-public class App extends Application {
+public class App extends LitePalApplication {
 
     public static Context mContext;
 
