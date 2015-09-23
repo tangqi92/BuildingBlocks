6f74f94dec1afc63106897741cfd3947f0042ac5
Troy Liu <troyliu0105@outlook.com>
2015-09-23 15:14:41 +0800

    move Serialization Method to model

--- app/src/main/java/me/itangqi/buildingblocks/domin/utils/CommonUtils.java
+++ app/src/main/java/me/itangqi/buildingblocks/domin/utils/CommonUtils.java
@@ -27,34 +27,4 @@ public class CommonUtils {
         return toolbarHeight;
     }
 
-    public static void serializDaily(String date, List<Daily> dailyList) throws IOException {
-        String itemParentPath = App.getContext().getCacheDir().getAbsolutePath() + "/daily";
-        Log.i("itemPath", itemParentPath);
-        File itemParent = new File(itemParentPath);
-        if (!itemParent.exists()) {
-            boolean createdDir = itemParent.mkdir();
-            Log.d("serilizDaily", itemParent.getName() + (createdDir ? "创建成功" : "创建失败"));
-        }
-        File item = new File(itemParentPath + "/" + date);
-        FileOutputStream fos = new FileOutputStream(item);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(dailyList);
-        oos.close();
-    }
-
-    public static List<Daily> deserializDaily(String date) throws IOException, ClassNotFoundException {
-        String cachePath = App.getContext().getCacheDir().getAbsolutePath();
-        FileInputStream fis = new FileInputStream(cachePath + "/daily/" + date);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        List<Daily> dailyList = (List<Daily>) ois.readObject();
-        ois.close();
-        return dailyList;
-    }
-
-    public static boolean hasSerializedObject(String date) {
-        String cachePath = App.getContext().getCacheDir().getAbsolutePath();
-        File file = new File(cachePath + "/daily/" + date);
-        return file.exists();
-    }
-
 }
--- app/src/main/java/me/itangqi/buildingblocks/model/DailyModel.java
+++ app/src/main/java/me/itangqi/buildingblocks/model/DailyModel.java
@@ -1,5 +1,7 @@
 package me.itangqi.buildingblocks.model;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -7,11 +9,17 @@ import com.loopj.android.http.BaseJsonHttpResponseHandler;
 
 import org.apache.http.Header;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import me.itangqi.buildingblocks.domin.api.ZhihuApi;
+import me.itangqi.buildingblocks.domin.application.App;
 import me.itangqi.buildingblocks.model.entity.Daily;
 import me.itangqi.buildingblocks.model.entity.DailyResult;
 import me.itangqi.buildingblocks.domin.utils.CommonUtils;
@@ -64,8 +72,8 @@ public class DailyModel implements IDaily {
     @Override
     public void getFromCache(String date) {
         try {
-            if (CommonUtils.hasSerializedObject(date)) {
-                mDailiesFromCache = CommonUtils.deserializDaily(date);
+            if (hasSerializedObject(date)) {
+                mDailiesFromCache = deserializDaily(date);
             }
             mCallBack.onFinish(mDailiesFromCache);
         } catch (IOException | ClassNotFoundException e) {
@@ -77,11 +85,40 @@ public class DailyModel implements IDaily {
     @Override
     public void saveDailies(List<Daily> dailies, String date) {
         try {
-            CommonUtils.serializDaily(date, dailies);
+            serializDaily(date, dailies);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void serializDaily(String date, List<Daily> dailyList) throws IOException {
+        String itemParentPath = App.getContext().getCacheDir().getAbsolutePath() + "/daily";
+        Log.i("itemPath", itemParentPath);
+        File itemParent = new File(itemParentPath);
+        if (!itemParent.exists()) {
+            boolean createdDir = itemParent.mkdir();
+            Log.d("serilizDaily", itemParent.getName() + (createdDir ? "创建成功" : "创建失败"));
+        }
+        File item = new File(itemParentPath + "/" + date);
+        FileOutputStream fos = new FileOutputStream(item);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(dailyList);
+        oos.close();
+    }
+
+    private List<Daily> deserializDaily(String date) throws IOException, ClassNotFoundException {
+        String cachePath = App.getContext().getCacheDir().getAbsolutePath();
+        FileInputStream fis = new FileInputStream(cachePath + "/daily/" + date);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        List<Daily> dailyList = (List<Daily>) ois.readObject();
+        ois.close();
+        return dailyList;
+    }
+
+    private boolean hasSerializedObject(String date) {
+        String cachePath = App.getContext().getCacheDir().getAbsolutePath();
+        File file = new File(cachePath + "/daily/" + date);
+        return file.exists();
+    }
 
 }
